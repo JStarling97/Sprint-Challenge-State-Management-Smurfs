5baf535a6884be5ccb7d20a2b4211bdb38541f54
Joshua Starling <joshstarling97@gmail.com>
2019-11-22 22:19:52 -0500

    App.js Setup. Gonna start working on components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:333/smurfs")
+      .then(res => {
+        setSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const addSmurfs = smurf => {
+    axios
+      .post("http://localhost:333/smurfs", smurf)
+      .then(res => {})
+      .catch(err => console.log(err));
+  };
+
+  const deleteSmurf = smurfId => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(res => {
+        setSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <h1>Enter Smurf Here. Lel </h1>
+    </div>
+  );
+};
 
 export default App;
